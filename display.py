26fc607b264a1a899a53c3700dd9e445f54ed48e
seanwascoding <a225108347171@gmail.com>
2023-11-18 06:14:09 -0800

    add sys.argv feature

--- display.py
+++ display.py
@@ -1,14 +1,18 @@
 import cv2
 import os
+import sys
 
-image_folder = './result/'
+if len(sys.argv)>1:
+    image_folder = sys.argv[1]
+else:
+    image_folder = './result-1116/result-2'
 
 image_files = sorted([os.path.join(image_folder, img) for img in os.listdir(image_folder) if img.endswith(".bmp")])
 
 print(len(image_files))
 
 cv2.namedWindow("Image Stream", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Image Stream", 416, 416)
+cv2.resizeWindow("Image Stream", 720, 720)
 
 for image_file in image_files:
     frame = cv2.imread(image_file)
